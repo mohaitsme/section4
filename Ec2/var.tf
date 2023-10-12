e4226634a09bff315b6c93dc9b9d41defb11e17b
mohaitsme <123353287+mohaitsme@users.noreply.github.com>
2023-10-12 19:40:06 +0530

    Updated var.tf

--- Ec2/var.tf
+++ Ec2/var.tf
@@ -1,8 +1,24 @@
-variable "ami_id" {}
-variable "aws_account_id" {}
-variable "default_cooldown" {}
-variable "health_check_grace_period" {}
-variable "alb_listener_port" {}
-variable "alb_listener_protocol" {}
-variable "key_name" {}
-variable "zoneidentifier" {}
+variable "ami_id" {
+  description = "AMI ID"
+}
+variable "aws_account_id" {
+  description = "Account ID"
+}
+variable "default_cooldown" {
+  description = "Default Cooldown"
+}
+variable "health_check_grace_period" {
+  description = "Grace period Healthcheck"
+}
+variable "alb_listener_port" {
+  description = "Load balancer listener port"
+}
+variable "alb_listener_protocol" {
+  description  = "Listener Protocol"
+}
+variable "key_name" {
+  description  = "Keyname"
+}
+variable "zoneidentifier" {
+  description  = "Zone identifier"
+}
